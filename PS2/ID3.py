35896d3af53f1dd0da22f45d8299c23eb58ea2c9
LuoLei_Zhao <luoleizhao2018@u.northwestern.edu>
2017-04-11 23:10:26 -0500

    added some functions for calculating infogain

--- PS2/ID3.py
+++ PS2/ID3.py
@@ -43,6 +43,44 @@ def ID3(examples, default):
 def choose_attribute(examples):
   return []
 
+def gain(S, A):
+  ent = entropy(S)
+  sumA = 0
+  for v in A:
+    Sv = {}
+    for val in S:
+      if (val[A] && val[A] == v)
+        Sv.append(val)
+    sumA += (len(Sv)/len(S)) * entropy(Sv)
+  return ent - sumA
+
+def entropy(examples):
+  ent = 0
+  attri_frequencies = {}
+  for key, val in examples.iteritems():
+  for val in attri_frequencies:
+    ent += (-val/len(examples)) * math.log((val/len(examples)),2)
+
+  def entropy(data, target_attr):
+    """
+    Calculates the entropy of the given data set for the target attribute.
+    """
+    val_freq     = {}
+    data_entropy = 0.0
+
+    # Calculate the frequency of each of the values in the target attr
+    for record in data:
+        if (val_freq.has_key(record[target_attr])):
+            val_freq[record[target_attr]] += 1.0
+        else:
+            val_freq[record[target_attr]]  = 1.0
+
+    # Calculate the entropy of the data for the target attribute
+    for freq in val_freq.values():
+        data_entropy += (-freq/len(data)) * math.log(freq/len(data), 2) 
+        
+    return data_entropy
+
 def mode(examples):
   countList = []
   maxCount = 0
